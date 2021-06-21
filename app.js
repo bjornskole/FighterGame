59e97efa1fe83669e0dc4e4caabd458e6b2e2205
Bjorn Sveia <bjorn@radalabs.net>
2021-06-21 13:24:02 +0200

    Update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // model
 
 const player = {
-  playerVal: 200,
-  playerHealt: 200,
-  attackPower: 5,
+  healtVal: 100,
+  HealtBarIndex: 40,
+  HealtBar: 40,
+  attackPower: 25,
   anim: {
     punch: {
       pic1: `<img class="player" src="./assets/player/Karakter1.png" /> `,
@@ -14,9 +15,10 @@ const player = {
   },
 };
 const computer = {
-  playerVal: 200,
-  playerHealt: 200,
-  attackPower: 5,
+  healtVal: 150,
+  HealtBarIndex: 40,
+  HealtBar: 40,
+  attackPower: 35,
   anim: {
     punch: {
       pic1: `<img class="computer" src="./assets/computer/CkBlue1.png" /> `,
@@ -26,9 +28,9 @@ const computer = {
     shot: "./assets/computer/Cfireball2.png",
   },
 };
-const some = `<div>hello</div>`;
-
+let chosenArena = false;
 let playerPic = player.anim.punch.pic1;
+let computerPic = computer.anim.punch.pic1;
 let playerShot = {
   pic: player.anim.shot,
   styleType: "height",
@@ -41,8 +43,7 @@ let computerShot = {
   styleOption: "right",
   move: 0,
 };
-let computerPic = computer.anim.punch.pic1;
-
+let callBack = "";
 const arena = {
   arena1: `<img class="arena" src="http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/emBDw7Sa.gif" />`,
   arena2: `<img class="arena" src="http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/HfCURPUa.gif" />`,
@@ -50,27 +51,59 @@ const arena = {
 // View
 app();
 function app() {
-  document.getElementById("Team").innerHTML = `
+  chosenArena === false
+    ? (document.getElementById("Team").innerHTML = `
+    <div class="frontPage" >
+    <h1 class="header">Choose arena</h1>
+    <div class="arenaBtns" >
+    <button onclick="choseArena(arena.arena1)"><img class="arenaBtn" src="http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/emBDw7Sa.gif" /></button>
+    <button onclick="choseArena(arena.arena2)"><img class="arenaBtn" src="http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/HfCURPUa.gif" /></button>
+    </div>
+    </div>
+    `)
+    : player.HealtBar <= 0
+    ? ((document.getElementById("Team").innerHTML = `
+    <div class="score" style="color: red">
+       You loose
+       <button onclick="location.reload();">Play again</button>
+    </div>
+    `),
+      (computer.HealtBar += 1))
+    : computer.HealtBar <= 0
+    ? ((document.getElementById("Team").innerHTML = `
+    <div class="score" style="color: green">
+       You Win
+       <button onclick="location.reload();">Play again</button>
+    </div>
+    `),
+      (player.HealtBar += 1))
+    : (document.getElementById("Team").innerHTML = `
     <div class="page">
         <div class="mainContainer">
         <div class="top-section">
-        ${arena.arena1}
+        ${chosenArena}
         ${playerPic}
         <img  id="plShot" class="shot" style="${playerShot.styleType}: ${playerShot.move}%"  src="${playerShot.pic}" />
+        <div class="playerHealtBar" style="width: ${player.HealtBar}%" ></div>
         ${computerPic}
         <img class="cshot" style="${computerShot.styleType}: ${computerShot.move}%"  src="${computerShot.pic}" />
+        <div class="computerHealtBar" style="width: ${computer.HealtBar}%"></div>
         </div>
+        <div class="btnss">
+        <p class="callback">${callBack}</p>
         <div class="btnCard">
-        <div class="playerHealtBar" style="width: ${computer.computerHealt}"></div>
-        <button onclick="attack()">attack</button>
+        <button onclick="attack()">Attack</button>
+        <button id="blck" onclick="block()">Block</button>
+        </div>
         </div>
         </div>
     </div>
-    `;
+    `);
 }
 
 // Controller
 function attack() {
+  callBack = "";
   setTimeout(function () {
     playerPic = player.anim.punch.pic2;
     app();
@@ -83,11 +116,49 @@ function attack() {
   setTimeout(function () {
     playerPic = player.anim.punch.pic1;
     computerAttack();
+    healthHandler(computer, player);
     app();
   }, 1500);
   app();
 }
-// new
+//
+function block() {
+  callBack = "";
+  let random = Math.floor(Math.random() * 2);
+  random === 1
+    ? (setTimeout(function () {
+        computerPic = computer.anim.punch.pic2;
+        app();
+      }, 200),
+      setTimeout(function () {
+        computerPic = computer.anim.punch.pic3;
+        shotHandler(computerShot);
+        app();
+      }, 1000),
+      setTimeout(function () {
+        computerPic = computer.anim.punch.pic1;
+        healthHandler(player, computer, "increment");
+        callBack = "perfect block! you gain health";
+        app();
+      }, 1500),
+      app())
+    : (setTimeout(function () {
+        computerPic = computer.anim.punch.pic2;
+        app();
+      }, 200),
+      setTimeout(function () {
+        computerPic = computer.anim.punch.pic3;
+        shotHandler(computerShot);
+        app();
+      }, 1000),
+      setTimeout(function () {
+        computerPic = computer.anim.punch.pic1;
+        healthHandler(player, computer, "no-increment");
+        callBack = "Missed block! you loose health";
+        app();
+      }, 1500),
+      app());
+}
 
 //
 function computerAttack() {
@@ -104,6 +175,11 @@ function computerAttack() {
   }, 1800);
   setTimeout(function () {
     computerPic = computer.anim.punch.pic1;
+    let randomShot = Math.floor(Math.random() * 5);
+    console.log(randomShot);
+    randomShot === 2
+      ? ((callBack = "Computer missed"), app())
+      : (healthHandler(player, computer), app());
     app();
   }, 2500);
   app();
@@ -121,17 +197,24 @@ function shotHandler(val) {
       : ((val.move += 10), app());
   }
 }
-function computerS() {
-  let id = setInterval(frame, 100);
-  function frame() {
-    computerShot.move > 70
-      ? (clearInterval(id), (computerShot.move = 18), app())
-      : ((computerShot.move += 10), app());
-  }
-}
 
 function Shotcalibrator(item, val) {
   val === "disable"
     ? ((item.styleType = "height"), (item.move = 0))
     : ((item.styleType = item.styleOption), (item.move = 18));
+}
+
+function healthHandler(val, secVal, indec) {
+  indec === "increment" && val.HealtBar <= 35
+    ? (val.HealtBar =
+        val.HealtBar + (secVal.attackPower * val.HealtBarIndex) / val.healtVal)
+    : indec === "no-increment" && val.HealtBar <= 35
+    ? (val.HealtBar = val.HealtBar - 5)
+    : (val.HealtBar =
+        val.HealtBar - (secVal.attackPower * val.HealtBarIndex) / val.healtVal);
+  app();
+}
+function choseArena(value) {
+  chosenArena = value;
+  app();
 }
