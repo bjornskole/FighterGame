61b427e08f01a98c5b95c5a72a174123b3f7b2f2
Bjorn Sveia <bjorn@radalabs.net>
2021-06-21 09:55:29 +0200

    update

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
 // model
+
 const player = {
-  playerHealt: "100%",
-  attackPower: "10%",
+  playerVal: 200,
+  playerHealt: 200,
+  attackPower: 5,
+  anim: {
+    punch: {
+      pic1: `<img class="player" src="./assets/player/Karakter1.png" /> `,
+      pic2: `<img class="player" src="./assets/player/Karakter2.png" /> `,
+      pic3: `<img class="player" src="./assets/player/Karakter3.png" /> `,
+    },
+    shot: "./assets/player/fireball2.png",
+  },
 };
 const computer = {
-  difficulty: easy,
-  computerHealt: "100%",
-  attackPower: "20%",
+  playerVal: 200,
+  playerHealt: 200,
+  attackPower: 5,
+  anim: {
+    punch: {
+      pic1: `<img class="computer" src="./assets/computer/CkBlue1.png" /> `,
+      pic2: `<img class="computer" src="./assets/computer/CkBlue2.png" /> `,
+      pic3: `<img class="computer" src="./assets/computer/CkBlue3.png" /> `,
+    },
+    shot: "./assets/computer/Cfireball2.png",
+  },
 };
-const playerWeapons = [
-  {
-    weaponOne: item,
-    attackPower: "1%",
-  },
-  {
-    weaponTwo: item,
-    attackPower: "10%",
-    // attacksReamaining: 2,
-  },
-];
+const some = `<div>hello</div>`;
+
+let playerPic = player.anim.punch.pic1;
+let playerShot = {
+  pic: player.anim.shot,
+  styleType: "height",
+  styleOption: "left",
+  move: 0,
+};
+let computerShot = {
+  pic: computer.anim.shot,
+  styleType: "height",
+  styleOption: "right",
+  move: 0,
+};
+let computerPic = computer.anim.punch.pic1;
+
 const arena = {
-  arena1:
-    "http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/emBDw7Sa.gif",
-  arena2:
-    "http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/HfCURPUa.gif",
+  arena1: `<img class="arena" src="http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/emBDw7Sa.gif" />`,
+  arena2: `<img class="arena" src="http://imgs.abduzeedo.com/files/articles/wicked-fighting-game-background-gifs/HfCURPUa.gif" />`,
 };
 // View
 app();
@@ -31,10 +53,85 @@ function app() {
   document.getElementById("Team").innerHTML = `
     <div class="page">
         <div class="mainContainer">
-        <div class="arena"><img src="${arena.arena1}"/></div>
-        <div class="btnCard"></div>
+        <div class="top-section">
+        ${arena.arena1}
+        ${playerPic}
+        <img  id="plShot" class="shot" style="${playerShot.styleType}: ${playerShot.move}%"  src="${playerShot.pic}" />
+        ${computerPic}
+        <img class="cshot" style="${computerShot.styleType}: ${computerShot.move}%"  src="${computerShot.pic}" />
+        </div>
+        <div class="btnCard">
+        <div class="playerHealtBar" style="width: ${computer.computerHealt}"></div>
+        <button onclick="attack()">attack</button>
+        </div>
         </div>
     </div>
     `;
 }
+
 // Controller
+function attack() {
+  setTimeout(function () {
+    playerPic = player.anim.punch.pic2;
+    app();
+  }, 200);
+  setTimeout(function () {
+    playerPic = player.anim.punch.pic3;
+    shotHandler(playerShot);
+    app();
+  }, 1000);
+  setTimeout(function () {
+    playerPic = player.anim.punch.pic1;
+    computerAttack();
+    app();
+  }, 1500);
+  app();
+}
+// new
+
+//
+function computerAttack() {
+  playerPic = player.anim.punch.pic1;
+  app();
+  setTimeout(function () {
+    computerPic = computer.anim.punch.pic2;
+    app();
+  }, 1500);
+  setTimeout(function () {
+    computerPic = computer.anim.punch.pic3;
+    shotHandler(computerShot);
+    app();
+  }, 1800);
+  setTimeout(function () {
+    computerPic = computer.anim.punch.pic1;
+    app();
+  }, 2500);
+  app();
+}
+
+function shotHandler(val) {
+  Shotcalibrator(val);
+  let id = setInterval(frame, 100);
+  function frame() {
+    val.move > 70
+      ? (clearInterval(id),
+        (val.move = 18),
+        Shotcalibrator(val, "disable"),
+        app())
+      : ((val.move += 10), app());
+  }
+}
+function computerS() {
+  let id = setInterval(frame, 100);
+  function frame() {
+    computerShot.move > 70
+      ? (clearInterval(id), (computerShot.move = 18), app())
+      : ((computerShot.move += 10), app());
+  }
+}
+
+function Shotcalibrator(item, val) {
+  val === "disable"
+    ? ((item.styleType = "height"), (item.move = 0))
+    : ((item.styleType = item.styleOption), (item.move = 18));
+}
